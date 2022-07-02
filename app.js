79d2d413c67fb89da69b99d5d2abd6920e168990
Vipul <81670997+Vipul1432@users.noreply.github.com>
2022-07-02 10:38:50 +0530

    User-Profile-Card

---
+++ app.js
@@ -0,0 +1,7 @@
+$(function() {
+    "use strict";
+    $(".pCard_add").click(function() {
+        $(".pCard_card").toggleClass("pCard_on");
+        $(".pCard_add i").toggleClass("fa-minus");
+    });
+});
